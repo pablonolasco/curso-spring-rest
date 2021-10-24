e77db26781cef3c8c85489e8ee63cb42322dbd8c
Pablo <pablo.nolasco@live.com.mx>
2021-10-24 12:28:26 -0500

    Update Application.java
    bean explicito

--- 01-spring-core-2/src/main/java/mx/com/udemy/core/Application.java
+++ 01-spring-core-2/src/main/java/mx/com/udemy/core/Application.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import mx.com.udemy.core.profiles.IEnviromentService;
 import mx.com.udemy.core.scope.ExampleScope;
@@ -14,13 +15,19 @@ public class Application {
 	
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
+	@Bean
+	public String getApplicationName() {
+		return "Bean explicito";
+	}
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 	//	IEnviromentService enviromentService=applicationContext.getBean(IEnviromentService.class);
 		ExampleScope exampleScope=applicationContext.getBean(ExampleScope.class);
 		ExampleScope exampleScope2=applicationContext.getBean(ExampleScope.class);
+		String name=applicationContext.getBean(String.class);
 		log.info("scope {}",exampleScope.equals(exampleScope2));
 		log.info("scope {}",exampleScope==exampleScope2);
+		log.info("bean {}",name);
 	}
 
 }
